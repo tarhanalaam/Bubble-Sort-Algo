b514974d272ab2ca5cc8348cc2f6a79529eba637
Tarhan <tarhanalam023@gmail.com>
2026-09-12 16:31:39 +0600

    Fix Algo

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def bubble_sort(arr: list[object]) -> list[object]:
     end: int = len(new_arr)
     while swapping:
         swapping = False
-        for i in range(0, end):
-            if new_arr[i] > new_arr[i + 1]:
+        for i in range(1, end):
+            if new_arr[i - 1] > new_arr[i]:
                 temp = new_arr[i]
-                new_arr[i] = new_arr[i + 1]
-                new_arr[i + 1] = temp
+                new_arr[i] = new_arr[i - 1]
+                new_arr[i - 1] = temp
                 swapping = True
         end -= 1
     return new_arr
@@ -26,10 +26,10 @@ if __name__ == "__main__":
         first_time = time.time()
         print(bubble_sort(integers))
         second_time = time.time()
-        print(f"First run took: {round(second_time - first_time, 3)} seconds")
+        print(f"First run took: {round((second_time - first_time), 5)} seconds")
         third_time = time.time()
         print(bubble_sort(floats))
         fourth_time = time.time()
-        print(f"Second run took: {round(fourth_time - third_time, 3)} seconds")
+        print(f"Second run took: {round((fourth_time - third_time), 5)} seconds")
     except Exception as e:
         print(f"Oops something unexpected happened: {e}")
